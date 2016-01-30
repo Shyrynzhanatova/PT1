fd476978c16c0af89c406aa5ce3baaac720fa7fd
shyryn <shyryn.zhanatova@mail.ru>
2016-01-30 18:22:49 +0600

    complex number

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,29 +6,36 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
+    class Complex
+    {
+        public int a, b; // комплексное число
+        public Complex (int a, int b)
+        {
+            this.a = a;//передаем параметры
+            this.b = b;
+        }
+        public static Complex operator +(Complex c1, Complex c2) //суммирование с оператором +
+        {
+            Complex sum = new Complex(c1.a + c2.a, c2.b + c2.b); //само число
+            return sum;
+        }
+        public override string ToString()// с базовой билиотеки переводим в наш комплекс
+        {
+            return a + "/" + b;//возвращаем
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string[] token = Console.ReadLine().Split(); 
-            //тукен нужен для того чтобы разделить заданный набор чисел (по пробелу)
-            for (int i = 0; i < token.Length; i++) //пробегаемся по числам 
-            {
-                int cnt = 0; //счетчик делителей
-                int a = int.Parse(token[i]); // интпарс переводит стринг в строку 
-                for (int j = 1; j <= a; j++) //пробегаемся по делителям 
-                {
-                    if (a % j == 0) //если заданное число делится на какое то число, то счетчик снт увеличивается
-                    {
-                        cnt++; //счетчик увеличиватся на 1
-                    }
-                }
-                if (cnt < 3) //число делителей не должно быть больше 2
-                {
-                    Console.Write("{0} ", a); //выводим это число
-                }
-            }
-            Console.ReadKey();
+            int n1, n2, n3, n4; //создаем переменные 
+            n1 = Int32.Parse(Console.ReadLine());
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+            n4 = Int32.Parse(Console.ReadLine());
+            Complex first_complex = new Complex(n1, n2);//передаем переменные классу
+            Complex second_complex = new Complex(n1, n2);
+            Complex sum = first_complex + second_complex;//вызываем оператор +
         }
     }
 }
