795d3952937bc8e69bed41d0682f4961673105d1
shyryn <shyryn.zhanatova@mail.ru>
2016-01-30 16:40:20 +0600

    prime number

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -10,10 +10,25 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string[] token = Console.ReadLine().Split(); 
+            //тукен нужен для того чтобы разделить заданный набор чисел (по пробелу)
+            for (int i = 0; i < token.Length; i++) //пробегаемся по числам 
+            {
+                int cnt = 0; //счетчик делителей
+                int a = int.Parse(token[i]); // интпарс переводит стринг в строку 
+                for (int j = 1; j <= a; j++) //пробегаемся по делителям 
+                {
+                    if (a % j == 0) //если заданное число делится на какое то число, то счетчик снт увеличивается
+                    {
+                        cnt++; //счетчик увеличиватся на 1
+                    }
+                }
+                if (cnt < 3) //число делителей не должно быть больше 2
+                {
+                    Console.Write("{0} ", a); //выводим это число
+                }
+            }
             Console.ReadKey();
-            //hello world program
-            /*hello world program */
         }
     }
 }
