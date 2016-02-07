c95da8a29420bcf8b65a47396b658af0fc7c2260
shyryn <shyryn.zhanatova@mail.ru>
2016-02-07 19:18:41 +0600

    directory tree

--- LAB2/LAB2/Program.cs
+++ LAB2/LAB2/Program.cs
@@ -9,20 +9,28 @@ namespace LAB2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void WalkDirectoryTree(DirectoryInfo d, int depth)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-
             try
             {
-                int c = a / b;
-                Console.WriteLine(c);
+                foreach (FileInfo file in d.GetFiles())
+                {
+                    Console.WriteLine("Depth={0}, File={1}", depth, file.Name);
+                }
+                foreach (DirectoryInfo di in d.GetDirectories())
+                {
+                    Console.WriteLine("Depth={0}, File={1}", depth, di.Name);
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"c:\windows");
+            WalkDirectoryTree(d, 0);
             Console.ReadKey();
         }
     }
