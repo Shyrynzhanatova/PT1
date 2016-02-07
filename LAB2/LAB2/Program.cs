8aa1da960e2d9e954076b6a031d9dddf5d4daa1c
shyryn <shyryn.zhanatova@mail.ru>
2016-02-07 19:07:58 +0600

    example of FileInfo

---
+++ LAB2/LAB2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("C:\\Users\\Shyryn\\Desktop");
+            if (file.Exists)
+            {
+                Console.WriteLine("File name {0}, file fullname {1}", file.Name, file.FullName);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            Console.ReadKey();
+        }
+    }
+}
